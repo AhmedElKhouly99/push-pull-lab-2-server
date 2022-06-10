6538ec6932cd7d45bb1493586f3322ef6f897f34
AhmedElKhouly99 <96910732+AhmedElKhouly99@users.noreply.github.com>
2022-06-11 00:44:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const express = require('express');
+const cors = require('cors');
+const { all } = require('express/lib/application');
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000"
+app.use(express.json());
+app.use(cors());
+
+const allMessages = [];
+
+
+app.post('/messages', (req, res) => {
+    console.log(req.body);
+    allMessages.push(req.body.msg);
+    res.sendStatus(204);
+});
+
+app.get('/messages', async (req, res) => {
+    const { len } = req.query;
+    console.log(len);
+    const newMsg = (allMessages.slice(len - allMessages.length));
+    res.send(newMsg);
+});
+
+const subscribers = {};
+const allMessagesLong = [];
+
+app.get('/messages-long', (req, res) => {
+    if (+req.query.all === 1) {
+        res.send(allMessagesLong)
+    } else {
+        const id = Math.ceil(Math.random() * 100000);
+        subscribers[id] = res;
     }
 });
 
-app.get('/', (req, res) => {
-    res.send("hellow");
-});
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('msg', (msg)=>{
-        console.log(msg);
-        socket.broadcast.emit('msgs', msg);
+app.post('/messages-long', async(req, res) => {
+    const { body } = req;
+    allMessagesLong.push(body.msg);
+    console.log(body);
+    Object.entries(subscribers).forEach(([id, response]) => {
+        response.json(body.msg);
+        delete subscribers[id];
     });
+    res.sendStatus(204);
 });
 
-server.listen(3010, () => {
-    console.log('listening on *:3010');
+
+app.listen(3001, () => {
+    console.log('chat server is running ...');
 });
